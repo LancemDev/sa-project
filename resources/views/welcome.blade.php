0a90f50ac4bad28b47a7b5fb5d4e2043b4f3b77f
LancemDev <munyaolance1@gmail.com>
2024-11-13 15:21:57 +0300

    Testing CI/CD

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Lancie Project Test 2</title>
+        <title>Lancie Project Test 3</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
